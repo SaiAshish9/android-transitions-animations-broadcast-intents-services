0e87adc7beb8e154d2ef9b7fa143758d493db372
saiashish9 <saiashish3760@gmail.com>
2021-02-18 15:32:48 +0530

    thread handlers updated

--- app/src/main/java/com/example/animationapp/MainActivity.java
+++ app/src/main/java/com/example/animationapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.animationapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +17,15 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity{
 
 
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            TextView tv=(TextView) findViewById(R.id.myText);
+            tv.setText("btn clicked");
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,23 +34,42 @@ public class MainActivity extends AppCompatActivity{
 
     public void ClickMyButton(View v){
 
-        long x = System.currentTimeMillis()+100000;
-
-        while (System.currentTimeMillis()<x){
-
-            synchronized (this){
-                try {
-
-                    wait(x-System.currentTimeMillis());
-
-                }catch (Exception e){
-
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                long x = System.currentTimeMillis()+10000;
+                while (System.currentTimeMillis()<x){
+                    synchronized (this){
+                        try {
+                            wait(x-System.currentTimeMillis());
+                        }catch (Exception e){ }
+                    }
                 }
+            handler.sendEmptyMessage(0);
             }
-        }
+        };
 
-        TextView tv=(TextView) findViewById(R.id.textView);
-        tv.setText("btn clicked");
+        Thread myThread = new Thread(r);
+        myThread.start();
+
+
+//        long x = System.currentTimeMillis()+10000;
+//        while (System.currentTimeMillis()<x){
+//            synchronized (this){
+//                try {
+//
+//                    wait(x-System.currentTimeMillis());
+//
+//                }catch (Exception e){
+//
+//                }
+//            }
+//        }
+
+
+
+        //        application crashes due to overload on main thread
+        //        which contains code which we need to execute inside another thread.
 
     }
 
